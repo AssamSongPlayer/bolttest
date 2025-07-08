16812f4aff1c87c10f222f620f0ed30b0227a5b2
AssamSongPlayer <AssamSongPlayer@gmail.com>
2025-07-08 11:25:09 +0530

    Update PlaylistsPage.tsx

--- components/PlaylistsPage.tsx
+++ components/PlaylistsPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ArrowLeft, Play, Music, Plus, MoreHorizontal, Edit2, Trash2 } from 'lucide-react';
 import { Playlist, Song } from '@/types';
-import { useTheme } from '@/app/page';
+import { useTheme } from '@/context/ThemeContext';
 import SongCard from './SongCard';
 
 interface PlaylistsPageProps {
